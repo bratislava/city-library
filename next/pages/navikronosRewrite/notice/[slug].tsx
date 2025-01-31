b9ae4f66a1b4155c34b3148eb4778174dec481f3
Radoslav Zeman <19418224+radoslavzeman@users.noreply.github.com>
2025-01-31 14:31:25 +0100

    #345 Unify newsletter.json with common.json

--- next/pages/navikronosRewrite/notice/[slug].tsx
+++ next/pages/navikronosRewrite/notice/[slug].tsx
@@ -91,7 +91,7 @@ export const getStaticProps: GetStaticProps<NoticePageProps, StaticParams> = asy
 
   const [general, translations, navikronosStaticProps] = await Promise.all([
     generalFetcher(locale),
-    serverSideTranslations(locale, ['common', 'forms', 'newsletter']),
+    serverSideTranslations(locale, ['common', 'forms']),
     navikronosGetStaticProps({
       navikronosConfig,
       ctx,
